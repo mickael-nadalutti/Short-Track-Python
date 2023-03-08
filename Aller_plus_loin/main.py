cff86d4513d24c28063c37a41b4f0c3dffbf763a
mickael-nadalutti <115152074+mickael-nadalutti@users.noreply.github.com>
2023-03-08 10:42:42 +0100

    Delete Aller_plus_loin directory

--- Aller_plus_loin/main.py
+++
@@ -1,6 +0,0 @@
-texte = "eioerjoiezijzoaiaizfihrind"
-
-if "e" in texte: 
-    print ("La chaîne de caractère contient au moins un 'e' ")
-else:
-    print ("La chaîne de caractère ne contient pas de 'e' ")
