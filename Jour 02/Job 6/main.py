69aa75266202564863f5a3d13131a1c677114fe8
mickael-nadalutti <mickael.nadalutti@laplateforme.io>
2023-03-08 10:34:56 +0100

    JOB6

---
+++ Jour 02/Job 6/main.py
@@ -0,0 +1,11 @@
+def number (nombre):
+    if (nombre >0):
+        return print (" positif")
+    elif (nombre <0 ):
+        return print ("negatif")
+    else :
+        return print ("neutre")
+
+number (1)
+number (-4)
+number (0)
