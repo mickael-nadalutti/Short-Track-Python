7a7fc290a5e077740b56fda65982b0ab70ac446a
mickael-nadalutti <mickael.nadalutti@laplateforme.io>
2023-03-08 10:41:44 +0100

    Jour 01

---
+++ Jour 01/Aller_plus_loin/main.py
@@ -0,0 +1,6 @@
+texte = "eioerjoiezijzoaiaizfihrind"
+
+if "e" in texte: 
+    print ("La chaîne de caractère contient au moins un 'e' ")
+else:
+    print ("La chaîne de caractère ne contient pas de 'e' ")
